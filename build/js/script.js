6a1acc08fc549d89d4c09958d5b1d20193eb1d40
Alecsandr <alecsandrcherneckiy@gmail.com>
2016-08-30 14:05:27 +0300

    pravka1

--- build/js/script.js
+++ build/js/script.js
@@ -12,12 +12,68 @@ $(document).ready(function() {
     } catch (err) {
 
     };
+/////////////////  desctop  ///////////////////
 
-    $('.tabs-nav').on('click', 'li:not(.active)', function() {
+    $('.tabs-nav_two').on('click', 'li:not(.active)', function() {
          $(this)
              .addClass('active').siblings().removeClass('active')
              .closest('.tabs').find('.tabs__item').removeClass('active').eq($(this).index()).addClass('active');
      });
+
+//////////////  table  //////////////
+
+$('.tabs-nav_tab').on('click', 'li:not(.active)', function() {
+     $(this)
+         .addClass('active').siblings().removeClass('active')
+         .closest('.tabs').find(".tabs-nav_tab__link").css({"display" : "block"});
+
+ });
+
+ $('.tabs-nav_tab').on('click', 'li.active', function() {
+      $(this)
+          .closest('.tabs').find('.tabs__item').removeClass('active').eq($(this).index()).addClass('active')
+          .closest('.tabs').find(".tabs-nav_tab__link").css({"display" : "none"});
+
+  });
+
+
+//////////////  mobail  //////////////
+
+$(window).bind("load resize", function () {
+  var width = $(this).width();
+
+  if(width <= 479){
+    $('.js-tab').removeClass("tabs-nav_two");
+    $('.js-tab').addClass("tabs-nav_one");
+
+    $(".tabs-nav_tab").addClass("tabs-nav__mob");
+  }else{
+    $('.js-tab').removeClass("tabs-nav_one");
+    $('.js-tab').addClass("tabs-nav_two");
+
+    $(".tabs-nav_tab").removeClass(".tabs-nav__mob");
+
+  }
+
+  $(".tabs-nav__mob").on('click', 'li:not(.active)', function() {
+       $(this)
+         .addClass('active').siblings().removeClass('active')
+         .closest('.tabs__nav').find(".tabs-nav_one").find('.tabs-nav__item')
+         .removeClass('active').eq($(this).index()).addClass('active');
+
+   });
+
+   $(".tabs-nav_one").on('click', 'li.active', function() {
+     $(this)
+         .addClass('active').siblings().removeClass('active')
+         .closest('.tabs').find('.tabs__item').removeClass('active').eq($(this).index()).addClass('active');
+
+    });
+
+
+});
+
+
 
 
 
@@ -62,7 +118,7 @@ $(document).ready(function() {
       //   })
       // };
 
-
+      //
       // if(window.innerWidth>480) {
       //
       //   $('#tabs-navs').removeClass('mob-tabs');
@@ -109,8 +165,8 @@ $(document).ready(function() {
       //
       //   }
       // });
-      //
-      //
+
+
 
 
 
